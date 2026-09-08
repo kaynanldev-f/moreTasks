ecb029aaec69ab00d5da86c2d9c99494c460e6bd
kaynanDev <kaynanldev@gmail.com>
2026-09-08 18:53:34 -0300

    feat: development of authentication functionality

--- src/app/components/header/Index.tsx
+++ src/app/components/header/Index.tsx
@@ -1,26 +1,42 @@
+"use client";
+
+import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 
 export default function Header() {
+  const { data: session } = useSession();
+
   return (
     <header className="bg-background w-full h-20 flex items-center justify-between px-6">
-      <div className="flex items-center justify-between gap-6">
-        <Link href="/" className="text-4xl font-bold text-foreground">
-          Tasks<span className="text-[#ea3140]">+</span>
-        </Link>
-      </div>
-      <nav className="flex items-center justify-between gap-6">
-        <Link
-          href="/dashboard"
-          className="bg-[#fafafa] py-1 px-4 rounded-sm text-background font-bold"
-        >
-          Meu painel
-        </Link>
-        <Link
-          href="/login"
-          className="border border-[#fafafa] py-1 px-4 rounded-sm text-[#fafafa] font-bold"
-        >
-          Acessar
-        </Link>
+      <Link href="/" className="text-4xl font-bold text-foreground">
+        Tasks<span className="text-[#ea3140]">+</span>
+      </Link>
+
+      <nav className="flex items-center gap-6">
+        {session && (
+          <Link
+            href="/dashboard"
+            className="bg-[#fafafa] py-1 px-4 rounded-sm text-background font-bold"
+          >
+            Meu painel
+          </Link>
+        )}
+
+        {session ? (
+          <button
+            onClick={() => signOut()}
+            className="border border-[#fafafa] py-1 px-4 rounded-sm text-[#fafafa] font-bold"
+          >
+            Sair
+          </button>
+        ) : (
+          <button
+            onClick={() => signIn("github")}
+            className="border border-[#fafafa] py-1 px-4 rounded-sm text-[#fafafa] font-bold"
+          >
+            Acessar
+          </button>
+        )}
       </nav>
     </header>
   );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
+import Providers from "./providers";
 import Header from "./components/header/Index";
 
 const roboto = Roboto({
@@ -18,8 +19,10 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
     <html lang="pt" className={`${roboto.variable} h-full antialiased`}>
       <body className="min-h-full flex flex-col">
         <>
-          <Header />
-          {children}
+          <Providers>
+            <Header />
+            {children}
+          </Providers>
         </>
       </body>
     </html>
